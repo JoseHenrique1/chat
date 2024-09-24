bb19f6dfe7a732908b0b7a140d8bc5c48ebb6f94
Jose Henrique <jh4832184@gmail.com>
2024-09-24 16:51:33 -0300

    fix: fix icons of search-bar

--- spa/src/components/search-bar.tsx
+++ spa/src/components/search-bar.tsx
@@ -1,4 +1,4 @@
-import { Undo2, Plus, Mic } from "lucide-react";
+import { Undo2, SendHorizonal } from "lucide-react";
 import { useContext } from "react";
 import { ResponsiveHomeContext } from "../contexts/responsive-home-context";
 
@@ -8,13 +8,12 @@ export function SearchBar() {
   return (
     <div className="bg-primary h-14 flex items-center gap-4 px-4 py-2">
       <Undo2 onClick={handleEnableContact} color="#1f2937" className="cursor-pointer md:hidden" />
-      <Plus color="#1f2937" className="cursor-pointer" />
       <input
         type="text"
         placeholder="Digite uma mensagem"
         className="flex-grow rounded bg-white text-gray-800 h-full px-4 text-sm focus:outline-none"
       />
-      <Mic color="#1f2937" className="cursor-pointer" />
+      <SendHorizonal color="#1f2937" className="cursor-pointer" />
     </div>
   );
 }
