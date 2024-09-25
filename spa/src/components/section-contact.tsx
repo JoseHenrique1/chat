622ba07dec86bdc1c4cc23e10206af14b229955f
Jose Henrique <jh4832184@gmail.com>
2024-09-25 16:25:25 -0300

    view: add userActions component in section-contact

--- spa/src/components/section-contact.tsx
+++ spa/src/components/section-contact.tsx
@@ -2,18 +2,20 @@ import { UserDefault } from "./user-deafult";
 import { Header } from "./header";
 import { Search } from "./search";
 import { ContactCardList } from "./contact-card-list";
-import { MoreVertical } from "lucide-react";
 import { BadgeList } from "./badge-list";
 import { ContactListFooter } from "./contact-list-footer";
+import { UserActions } from "./user-actions";
+
 
 export function SectionContact() {
+
   return (
     <div className="flex flex-col w-full min-h-screen md:max-w-sm">
       <Header>
         <div>
           <UserDefault width={40} height={40} />
         </div>
-        <MoreVertical size={20} color="#1f2937" className="cursor-pointer"/>
+        <UserActions/>
       </Header>
       <div className="px-4 py-2 flex flex-wrap gap-2 border-b border-gray-300">
         <Search />
---
+++ spa/src/components/user-actions.tsx
@@ -0,0 +1,53 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { MoreVertical, UserRoundPlus, Users } from 'lucide-react'
+import { useModal } from '../hooks/useModal'
+import { Modal } from './modal'
+
+export function UserActions() {
+  const friendModal = useModal()
+  const groupModal = useModal()
+
+  //<Menu __demoMode> ja vem com o dropdown ligado
+  return (
+    <>
+      <Menu>
+        <MenuButton className="inline-flex items-center gap-2 rounded-md bg-transparent py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
+          <MoreVertical size={20} color="#1f2937" className="cursor-pointer" />
+        </MenuButton>
+
+        <MenuItems
+          transition
+          anchor="bottom end"
+          className="w-52 origin-top-right rounded-xl border border-white/5 bg-black/90 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+        >
+          <MenuItem>
+            <div
+              onClick={friendModal.handleOpen}
+              className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+              <UserRoundPlus className="size-4 fill-white/30" />
+              Adicionar amigo
+              <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘E</kbd>
+            </div>
+          </MenuItem>
+          <MenuItem>
+            <button
+              onClick={groupModal.handleOpen}
+              className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+              <Users className="size-4 fill-white/30" />
+              Criar grupo
+              <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">HAHA</kbd>
+            </button>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
+
+      <Modal {...friendModal} title="Adicionar amigo">
+        Formulario para adicionar amigo
+      </Modal>
+      <Modal {...groupModal} title="Criar grupo">
+        Formulario para criar grupo
+      </Modal>
+    </>
+
+  )
+}
