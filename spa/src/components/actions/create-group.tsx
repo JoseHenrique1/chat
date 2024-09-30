d945e3e27577bdcb0e8fb2e3435aba94dbdb54d2
Jose Henrique <jh4832184@gmail.com>
2024-09-30 20:23:34 -0300

    feat: add hook form in create-group

--- spa/src/components/actions/create-group.tsx
+++ spa/src/components/actions/create-group.tsx
@@ -1,20 +1,70 @@
 import { Input } from "../input";
 import { Button } from "../button";
+import { FriendCheckbox } from "../friend-checkbox";
+import { useForm } from "react-hook-form";
+const friends = [
+  {
+    id: "1",
+    name: "jose",
+    email: "jose@gmail",
+    selected: false
+  },
+  {
+    id: "2",
+    name: "joao",
+    email: "joao@gmail",
+    selected: true
+  },
+  {
+    id: "3",
+    name: "maria",
+    email: "maria@gmail",
+    selected: false
+  }
+]
+
+interface CreateGroup {
+  name: string,
+  friends: { [key: string]: boolean };
+}
 
 export function CreateGroup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateGroup>()
+
+  async function onSubmit(data: CreateGroup) {
+    console.log(data);
+
+  }
+
+
   return (
-    <form className='flex flex-col gap-2 mt-2'>
-      <Input placeholder='Nome do grupo' />
-      <div>
-        <div className="flex gap-2">
-          <input type="checkbox" />
-          <label htmlFor="">amigo 1</label>
-        </div>
-        <div className="flex gap-2">
-          <input type="checkbox" />
-          <label htmlFor="">amigo 1</label>
-        </div>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className='flex flex-col gap-2 mt-2'>
+
+      <Input
+        {...register("name", { minLength: 3 })}
+        placeholder='Nome do grupo' />
+
+      <div className="flex flex-col gap-1 overflow-y-auto">
+        {friends.map((friend) => {
+          return <div key={friend.email}>
+            <label>
+              <input
+                type="checkbox"
+                {...register(`friends.${friend.email}`)}
+              />
+              {friend.email}
+            </label>
+          </div>
+
+        })}
       </div>
+
       <div className='flex justify-end'>
         <Button type='submit' >Salvar</Button>
       </div>
