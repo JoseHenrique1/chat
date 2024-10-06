e9219924fc3e5cfd9f9317d735af100a49890ce7
Jose Henrique <jh4832184@gmail.com>
2024-10-06 11:00:59 -0300

    feat: add router get auth/user

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { UserController } from "../user/user.controller";
 import { AuthService } from "./auth.service";
-import { authType } from "./auth.schema";
+import { authType, userAuth } from "./auth.schema";
+import { verify } from "jsonwebtoken";
+import { prisma } from "../../databse/prisma";
 
 async function sigin(request: FastifyRequest<{Body: authType}>, reply: FastifyReply) {
   const email = request.body.email
@@ -17,7 +19,35 @@ async function sigin(request: FastifyRequest<{Body: authType}>, reply: FastifyRe
   reply.send({token})
 }
 
+interface userInterface {
+  email: string,
+  name: string
+}
+
+async function user(request: FastifyRequest<{Headers: userAuth}>, reply: FastifyReply) {
+  const authorization = request.headers.authorization;
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    reply.code(401).send({ message: "Unauthorized" });
+  }
+
+  const token = authorization.split(" ")[1];
+  const decoded = verify(token, process.env.JWT_SECRET_KEY || "") as userInterface
+  
+  const user = await prisma.user.findUnique({
+    where: {
+      email: decoded.email
+    },
+    select: {
+      email: true,
+      name: true
+    }
+  })
+
+  reply.send(user)
+}
+
 export const AuthController = {
   sigin,
-  signup: UserController.createUser
+  signup: UserController.createUser,
+  user
 }
--- api/src/modules/auth/auth.route.ts
+++ api/src/modules/auth/auth.route.ts
@@ -6,6 +6,9 @@ import { AuthSchema } from "./auth.schema";
 export function AuthRoute(fastify: FastifyInstance, opts: any, done: HookHandlerDoneFunction) {
   fastify.post("/signin", AuthSchema.sigin,  AuthController.sigin)
 
+  //pegar dados do user logado
+  fastify.get("/user",  AuthController.user)
+
   fastify.post("/signup", AuthSchema.sigup , AuthController.signup)
 
   done()
