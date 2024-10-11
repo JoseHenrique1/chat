08d8e4c637289b298baaca56435491df565d12cc
Jose Henrique <jh4832184@gmail.com>
2024-10-11 09:33:42 -0300

    feat: add friend

--- spa/src/components/actions/add-friend.tsx
+++ spa/src/components/actions/add-friend.tsx
@@ -1,10 +1,53 @@
 import { Input } from "../input";
 import { Button } from "../common/button";
+import { useForm, SubmitHandler } from "react-hook-form"
+import { useContext } from "react";
+import { AuthenticationContext } from "../../contexts/authentication-context";
+import { friend } from "../../interfaces/contexts";
+import { ContactsContext } from "../../contexts/contacts-context";
+interface addFriendInterface {
+  email: string
+}
+
 
 export function AddFriend() {
+  const { setFriends } = useContext(ContactsContext)
+  const { api } = useContext(AuthenticationContext)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<addFriendInterface>()
+
+  function onSubmit(data: addFriendInterface) {
+    api.post<friend>("/friends", data)
+      .then((r) => {
+        if (r.status === 200 || r.status === 201) {
+          const friend = r.data
+          setFriends(prev => [...prev, friend])
+          return
+        }
+        alert("erro, verifique o email")
+      })
+      .catch(e => alert("erro, verifique o email"))
+
+
+  }
   return (
-    <form className='flex flex-col gap-2 mt-2'>
-      <Input placeholder='amigo@gmail.com' />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className='flex flex-col gap-2 mt-2'>
+      <Input
+        type="email"
+        {...register("email", {
+          required: "Campo obrigatório",
+          minLength: {
+            value: 10,
+            message: "Precisa ter no mínimo 10 caracteres"
+          }
+        })}
+        placeholder='amigo@gmail.com' />
+      {errors.email && <p>{errors.email.message}</p>}
       <div className='flex justify-end'>
         <Button type='submit' >Salvar</Button>
       </div>
