079526148fd481998ec9df1b892ea70ee90f97eb
Jose Henrique <jh4832184@gmail.com>
2024-10-12 16:53:18 -0300

    fix: fixing delay

--- spa/src/components/presentation.tsx
+++ spa/src/components/presentation.tsx
@@ -6,7 +6,7 @@ export function Presentation() {
   return (
     <div className="w-full flex justify-center items-start pt-16">
       <div className="backdrop-blur-xl p-4 rounded-md border lg:py-8 lg:px-12">
-        <h1 className="text-tertiary text-4xl lg:text-6xl">Olá, <span className="text-special">{user.name}</span> </h1>
+        <h1 className="text-tertiary text-4xl lg:text-6xl">Olá, <span className="text-special">{user?.name}</span> </h1>
         <p className="text-tertiary/85 font-medium text-lg">Abra um contato e inicie uma conversa!</p>
       </div>
     </div>
--- spa/src/hooks/useCheckToken.ts
+++ spa/src/hooks/useCheckToken.ts
@@ -3,23 +3,25 @@ import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export function useCheckToken() {
+  
   const location = useLocation()
   const navigate = useNavigate()
-  useEffect(()=>{
+  useEffect(() => {
     const token = Cookies.get("token")
-    const {pathname} = location
-    
-    if(!token && pathname === "/"){
+    const { pathname } = location
+
+
+    if (!token && pathname === "/") {
       navigate("/auth/signin")
       return
     }
-    if(token && pathname === "/auth/signin"){
+    if (token && pathname === "/auth/signin") {
       navigate("/")
       return
     }
-    if(token && pathname === "/auth/signup"){
+    if (token && pathname === "/auth/signup") {
       navigate("/")
       return
     }
-  },[])
+  }, [])
 }
